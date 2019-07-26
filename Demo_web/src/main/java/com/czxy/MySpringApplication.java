b0fa077551dcf898561434d8ab02e00b9f85a99b
744110657w <w744110657>
2019-07-26 09:19:37 +0800

    test

---
+++ Demo_web/src/main/java/com/czxy/MySpringApplication.java
@@ -0,0 +1,17 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Mr.Ren
+ * @version: v 1.0
+ * @date: 2019/7/25
+ * @info:
+ */
+@SpringBootApplication
+public class MySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApplication.class, args);
+    }
+}
